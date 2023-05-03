393c17f39de0ac928f569f324f66f3d4e9bec34b
natan-matos-souza <natanmatos_18@hotmail.com>
2023-05-03 11:20:10 -0300

    Terminando header

--- js/responsive.js
+++ js/responsive.js
@@ -6,16 +6,25 @@ const header = document.getElementById('header')
 
 menuBtn.addEventListener('click', function() {
     if (list.style.display == "none") {
+        list.style.animation = 'dropdownEffect 1s'
         list.style.display = "block"
     } else {
-        list.style.display = "none"
+        setTimeout(() => {
+            list.style.display = "none"
+        }, 950)
+        list.style.animation= 'dropInEffect 1s'
     }
 })
 
 
+menuBtn.addEventListener('dblclick', function() {
+    alert('Aperte apenas uma vez para realizar a animação !')
+})
+
 setInterval(() => {
 
     if (window.innerWidth > 995) {
+        list.style.animation = 'none'
         list.style.display = "block"
     }
 }, 100)
