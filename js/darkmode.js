5b93c617e8dc9a4ca3e812ffffaebce7dfa905dd
natan-matos-souza <natanmatos_18@hotmail.com>
2023-05-03 16:43:10 -0300

    Incrementando dark-mode

--- js/darkmode.js
+++ js/darkmode.js
@@ -1,6 +1,26 @@
-const darkmodeBtn = document.getElementById('dark-mode-icon')
 const bodyElement = document.getElementById('body')
+    
+if (localStorage.getItem('theme') == "dark") {
+    bodyElement.setAttribute('class', "dark-body")
+}
 
-darkmodeBtn.addEventListener('click', function() {
-    bodyElement.style.backgroundColor = '#393646'
+if (localStorage.getItem('theme') == "default") {
+    bodyElement.setAttribute('class', "light-body")
+}
+
+if (localStorage.getItem('theme') == null || localStorage.getItem('theme') == "") {
+    bodyElement.setAttribute('class', "light-body")
+    localStorage.setItem('theme', 'default' )
+}
+
+const darkModeBtn = document.getElementById('dark-mode-icon')
+
+darkModeBtn.addEventListener('click', function() {
+    if (localStorage.getItem('theme') == "default") {
+        bodyElement.setAttribute('class', "dark-body")
+        localStorage.setItem('theme', "dark")
+    } else {
+        bodyElement.setAttribute('class', "light-body")
+        localStorage.setItem('theme', "default")
+    }
 })
