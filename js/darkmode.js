5857e1871f5f425c0ccbc52809e5f1199170e4dc
L6Unip <L6Unip@SRVLAB.COM>
2023-05-09 20:44:57 -0300

    Implementando dark-mode

--- js/darkmode.js
+++ js/darkmode.js
@@ -3,10 +3,12 @@ const darkModeBtn = document.getElementById('dark-mode-icon')
 
 if (localStorage.getItem('theme') == "dark") {
     htmlElement.setAttribute('class', 'dark-mode')
+    darkModeBtn.innerHTML = "light_mode"
 }
 
 if (localStorage.getItem('theme') == "default") {
     htmlElement.setAttribute('class', 'light-mode')
+    darkModeBtn.innerHTML = "dark_mode"
 }
 
 if (localStorage.getItem('theme') == "" || localStorage.getItem('theme') == null) {
@@ -18,8 +20,10 @@ darkModeBtn.addEventListener('click', () => {
     if (htmlElement.getAttribute('class') == "light-mode") {
         htmlElement.setAttribute('class', 'dark-mode')
         localStorage.setItem('theme', 'dark')
+        darkModeBtn.innerHTML = "light_mode"
     } else {
         htmlElement.setAttribute('class', 'light-mode')
         localStorage.setItem('theme', 'default')
+        darkModeBtn.innerHTML = "dark_mode"
     }
 })
