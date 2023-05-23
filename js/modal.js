05c63a7f0649336bf20b2f7470fd8f5dc1e8b96a
natan-matos-souza <natanmatos_18@hotmail.com>
2023-05-22 23:12:51 -0300

    Criando modal

--- js/modal.js
+++ js/modal.js
@@ -1,15 +1,35 @@
 const modalDiv = document.getElementById('modal')
+const closeModalBtn = document.getElementsByClassName('close-modal-btn')
+
+//importing buttons
+
 const reuseBtn = document.getElementById('reuse-btn')
-const closeModalBtn = document.getElementById('close-modal')
+const recicleBtn = document.getElementById('recicle-btn')
+
+//Importing divs
+
+const recicleDiv = document.getElementById('recicle-div')
+const reuseDiv = document.getElementById('reuse-div')
 
 
 reuseBtn.addEventListener('click', () => {
-
-    modalDiv.style.display = "block"
-
+    modalDiv.style.display = 'block'
+    reuseDiv.style.display = 'block'
 })
 
-closeModalBtn.addEventListener('click', () => {
-
-    modalDiv.style.display = "none"
+recicleBtn.addEventListener('click', () => {
+    modalDiv.style.display = 'block'
+    recicleDiv.style.display = 'block'
 })
+
+function closeModal() {
+    modalDiv.style.display = 'none'
+    recicleDiv.style.display = 'none'
+    reuseDiv.style.display = 'none'
+}
+
+closeModalBtn[0].addEventListener('click', closeModal)
+closeModalBtn[1].addEventListener('click', closeModal)
+closeModalBtn[3].addEventListener('click'. closeModal)
+
+console.log(closeModalBtn.length)
